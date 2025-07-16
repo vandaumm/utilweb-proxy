b1931b3600d6cf9e438d17491e9622f9e481a0ae
vandaumm <112284197+vandaumm@users.noreply.github.com>
2025-07-16 17:21:56 -0300

    Update proxy.js

--- api/proxy.js
+++ api/proxy.js
@@ -28,7 +28,7 @@ export default async function handler(req, res) {
         "Content-Type": "application/json"
       },
       body: JSON.stringify({
-        inputs: [{ role: "user", content: prompt }],
+        inputs: prompt,
         parameters: {
           max_new_tokens: 200,
           return_full_text: false,
